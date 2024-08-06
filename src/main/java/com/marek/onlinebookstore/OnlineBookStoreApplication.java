56c75a335c4cdac9d8522beafc6605c69ba660c3
mgucma <marek.gucm@gmail.com>
2024-08-06 12:45:49 +0200

    fix

--- src/main/java/com/marek/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/marek/onlinebookstore/OnlineBookStoreApplication.java
@@ -2,12 +2,10 @@ package com.marek.onlinebookstore;
 
 import com.marek.onlinebookstore.entity.Book;
 import com.marek.onlinebookstore.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import java.math.BigDecimal;
 
 @SpringBootApplication
@@ -20,8 +18,10 @@ public class OnlineBookStoreApplication {
     @Bean
     CommandLineRunner initDatabase(BookService bookService) {
         return args -> {
-            bookService.save(new Book(null, "Book Title 1", "Author 1", "ISBN001", new BigDecimal("9.99"), "Description 1", "cover1.jpg"));
-            bookService.save(new Book(null, "Book Title 2", "Author 2", "ISBN002", new BigDecimal("19.99"), "Description 2", "cover2.jpg"));
+            bookService.save(new Book(null, "Book Title 1", "Author 1", "ISBN001",
+                    new BigDecimal("9.99"), "Description 1", "cover1.jpg"));
+            bookService.save(new Book(null, "Book Title 2", "Author 2", "ISBN002",
+                    new BigDecimal("19.99"), "Description 2", "cover2.jpg"));
         };
     }
 }
--- src/main/java/com/marek/onlinebookstore/service/BookService.java
+++ src/main/java/com/marek/onlinebookstore/service/BookService.java
@@ -1,10 +1,11 @@
 package com.marek.onlinebookstore.service;
 
 import com.marek.onlinebookstore.entity.Book;
-
 import java.util.List;
 
 public interface BookService {
+
     Book save(Book book);
+
     List<Book> findAll();
 }
